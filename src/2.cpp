32c121cc93decbd80c4012b940849ab1b012d0ee
b6osigmund <b6osigmund@gmx.com>
2025-03-04 14:19:40 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,12 @@
+
+int main() {
+    int num = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (num % 2 == 0) {
+        cout << "Even" << endl;
+    } else {
+        cout << "Odd" << endl;
+    }
+    return 0;
+}
